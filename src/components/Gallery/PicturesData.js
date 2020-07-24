333173a209e0d04f4ef496e0dce7e199ae3cec8a
Lucas <tnrlucas@gmail.com>
2020-07-24 05:14:32 -0300

    commit 4

--- src/components/Gallery/PicturesData.js
+++ src/components/Gallery/PicturesData.js
@@ -1,41 +1,41 @@
 const data =
-  [
-    {
-      name: "Cookouts",
-      imgs:
-        [
-          "https://i.ibb.co/SVbYkPc/IMG-2771.jpg",
-          "https://i.ibb.co/QmK2hkv/IMG-2772.jpg",
-          "https://i.ibb.co/1688pq5/IMG-2778.jpg"
+[
+  {
+    name: "Cookouts",
+    imgs:
+      [
+        "https://i.ibb.co/SVbYkPc/IMG-2771.jpg",
+        "https://i.ibb.co/QmK2hkv/IMG-2772.jpg",
+        "https://i.ibb.co/1688pq5/IMG-2778.jpg"
+      ]
+  },
+  {
+    name: "Dante's Inferno",
+    imgs:
+      [
+        "https://www.youtube.com/embed/ZbcATHJkrGM"
+      ]
+  },
+  {
+    name: "Intramurals",
+    imgs:
+      [
+        "https://i.ibb.co/QH2Xsyz/IMG-2779.jpg",
+        "https://i.ibb.co/L0RhX7M/IMG-2780.jpg",
+        "https://i.ibb.co/SJMcNDD/IMG-2781.jpg",
+        "https://i.ibb.co/dbkHRks/IMG-2782.jpg",
+        "https://i.ibb.co/jR6P4f7/IMG-2783.jpg"
+      ]
+  },
+  {
+    name: "BMS Banquet",
+    imgs:
+      [
+        "https://i.ibb.co/pWjfW9K/IMG-2773.jpg",
+        "https://i.ibb.co/8rvKwxz/IMG-2784.jpg",
+        "https://i.ibb.co/WGX2bHW/IMG-2785.jpg"
         ]
-    },
-    {
-      name: "Dante's Inferno",
-      imgs:
-        [
-          "https://www.youtube.com/embed/ZbcATHJkrGM"
-        ]
-    },
-    {
-      name: "Intramurals",
-      imgs:
-        [
-          "https://i.ibb.co/QH2Xsyz/IMG-2779.jpg",
-          "https://i.ibb.co/L0RhX7M/IMG-2780.jpg",
-          "https://i.ibb.co/SJMcNDD/IMG-2781.jpg",
-          "https://i.ibb.co/dbkHRks/IMG-2782.jpg",
-          "https://i.ibb.co/jR6P4f7/IMG-2783.jpg"
-        ]
-    },
-    {
-      name: "BMS Banquet",
-      imgs:
-        [
-          "https://i.ibb.co/pWjfW9K/IMG-2773.jpg",
-          "https://i.ibb.co/8rvKwxz/IMG-2784.jpg",
-          "https://i.ibb.co/WGX2bHW/IMG-2785.jpg"
-          ]
-    }
-  ]
+  }
+]
 
 export default data
